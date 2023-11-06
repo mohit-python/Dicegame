f429a7a9742f57ad1eda1f67217017e5229539a4
mohit-python <93514876+mohit-python@users.noreply.github.com>
2023-11-06 19:51:58 +0530

    removed the images folder

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var randomNumber1 = Math.floor(Math.random()*6)+1;
 var randomNumber2 = Math.floor(Math.random()*6)+1;
 var randomdiceimage1 = "dice"+randomNumber1+".png";
-var randomimage1 = "images/"+randomdiceimage1;
+var randomimage1 = randomdiceimage1;
 var image1 = document.querySelectorAll("img")[0];
 image1.setAttribute("src",randomimage1);
 var randomdiceimage2 = "dice"+randomNumber2+".png";
-var randomimage2 = "images/"+randomdiceimage2;
+var randomimage2 = randomdiceimage2;
 var image2 = document.querySelectorAll("img")[1];
 image2.setAttribute("src",randomimage2);
 if(randomNumber1>randomNumber2)
